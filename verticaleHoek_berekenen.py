b0b73fc46bead96c75f98cbd3b27405312fb42d2
Jesse Lema <jesse.lema@student.kuleuven.be>
2023-03-24 15:26:15 +0100

    code

--- verticaleHoek_berekenen.py
+++ verticaleHoek_berekenen.py
@@ -6,25 +6,24 @@ input:
 """
 
 waterDebiet = 3.95  # l/min (3.95 is standaardwaarde), komt van waterflowsensor
-afstandBeker = 10  # komt van webcam
+afstandBeker = 10.44  # komt van webcam
 volumeGepasseerd = 0  # ml, komt van waterflowsensor
 
 def hoekV(waterDebiet, afstandBeker):
 
-    straal = 0.0015
-    hoogteDoos = 0.5
-    lengteArm = 0.32
-    l0 = 0.05  # lengte tussen beginpunt arm en camera
+    straal = 0.0029
+    hoogteDoos = 1
+    lengteArm = 0.48
+    l0 = 0.02  # lengte tussen beginpunt arm en camera
 
     hoogteBeker = 0
 
-    snelheid = (waterDebiet * 1,6667*10**(-5)) / (np.pi * (straal ** 2))
+    snelheid = (waterDebiet * 1.6667*10**(-5))/(np.pi * (straal ** 2))
     def func(a):
         return [-afstandBeker + np.cos(a[0]) * snelheid * a[1] + np.cos(a[0]) * lengteArm - l0,
-                -hoogteBeker + hoogteDoos + np.sin(a[0]) * lengteArm + np.sin(a[0]) * snelheid * a[1] - 1 / 2 * 9.81 * (
-                            a[1] ** 2)]
+                -hoogteBeker + hoogteDoos + np.sin(a[0]) * lengteArm + np.sin(a[0]) * snelheid * a[1] - 1 / 2 * 9.81 * (a[1]**2)]
 
-    opl = fsolve(func, [np.pi / 2, 100])
+    opl = fsolve(func, [np.pi/2, 10])
     opl[0] = opl[0] * 180 / np.pi
     #opl[0]: hoek (in graden)
     return opl[0]
@@ -38,4 +37,4 @@ def stoppenPomp(volumeGepasseerd):
 
 
 
-
+print(hoekV(waterDebiet, afstandBeker))
