835a60c6521ac486f64690f1bbce961d06ebfb44
annalauradeclercq <anna-laura.declercq@student.kuleuven.be>
2023-03-31 01:03:36 +0200

    tijdelijk groepsverslag finale versie
    @jesse er staan een paar opmerkingen in het document over tekst die we nog zouden kunnen toevoegen

--- verticaleHoek_berekenen.py
+++ verticaleHoek_berekenen.py
@@ -15,18 +15,18 @@ def hoekV(waterDebiet, afstandBeker):
     hoogtePlatform = 0.5
     lengteArm = 0.48
     afstandCamera = 0.02  # lengte tussen beginpunt arm en camera
-
     hoogteBeker = 0
 
     snelheid = (waterDebiet * 1.6667*10**(-5))/(np.pi * (straal ** 2))
+    print(snelheid)
     def func(a):
         return [-afstandBeker + np.cos(a[0]) * snelheid * a[1] + np.cos(a[0]) * lengteArm - afstandCamera,
                 -hoogteBeker + hoogtePlatform + np.sin(a[0]) * lengteArm + np.sin(a[0]) * snelheid * a[1] - 1 / 2 * 9.81 * (a[1]**2)]
 
-        opl = fsolve(func, [np.pi/2, 10])
-        opl[0] = opl[0] * 180 / np.pi
-        #opl[0]: hoek (in graden)
-        return opl[0]
+    opl = fsolve(func, [np.pi/2, 10])
+    opl[0] = opl[0] * 180 / np.pi
+    #opl[0]: hoek (in graden)
+    return opl[0]
 
 
 def stoppenPomp(volumeGepasseerd):
